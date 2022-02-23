5b433cf7a19d872ac22691bec4868466cb4dc5ba
BluBloos <cnoah1705@gmail.com>
2022-02-23 17:41:35 -0500

    MANO layer patched to not fail on file not found

--- src/mano_layer.py
+++ src/mano_layer.py
@@ -219,122 +219,126 @@ class MANO_Model(Model):
     
     # --- Load in learned mano paramaters ---
     file_path = os.path.join(mano_dir, "models", "MANO_RIGHT.pkl")
-    manoRight = pickle.load(open(file_path, 'rb'), encoding='latin1')
-
-    self.V = manoRight['v_template']          # Vertices of template model (V), Shape=(778, 3), type=uint32
-    self.F = manoRight['f']                   # Faces of the model (F), Shape=(1538, 3), type=uint32
-    self.K = manoRight['kintree_table'][0]   # Kinematic tree defining the parent joint (K), Shape=(16,), type=int64
-    self.S = manoRight['shapedirs']           # Shape blend shapes that are learned (S), Shape=(778, 3, 10), type=float64
-    self.P = manoRight['posedirs']            # Pose blend shapes that are learned (P), Shape=(778, 3, 135), type=float64
-    self.J = manoRight['J_regressor']         # Joint regressor that are learned (J), Shape=(16,778), type=float64
-    self.W = manoRight['weights']             # Weights that are learned (W), Shape=(778,16), type=float64
-    self.C = manoRight['hands_components']    # Components of hand PCA (C), Shape=(45, 45), type=float64
-    self.M = manoRight['hands_mean']          # Mean hand PCA pose (M), Shape=(45,), type=float64
-
-    # Convert loaded params to Numpy
-    self.V = np.array(self.V, dtype=np.float32)
-    self.F = np.array(self.F, dtype=np.int32) # Need to convert from uint32 to int32 to allow interation in v0 = vertices[:, self.F[:,0],:] # [bs, 1538, 3]    
-    self.S = np.array(self.S, dtype=np.float32)
-    self.P = np.array(self.P, dtype=np.float32)
-    self.J = np.array(self.J.todense(), dtype=np.float32) # Need to convert sparse to dense matrix
-    self.W = np.array(self.W, dtype=np.float32)
-    self.C = np.array(self.C, dtype=np.float32) # Use PCA
-    self.M = np.array(self.M, dtype=np.float32)
-    self.K[0] = -1 # Convert undefined parent of root joint to -1
     
-    # Reshape the pose blend shapes from (778, 3, 135) -> (778*3, 135) -> (135, 778*3)
-    num_pose_basis = self.P.shape[-1]
-    P = np.reshape(self.P, [-1, num_pose_basis]).T
+    try:
+      manoRight = pickle.load(open(file_path, 'rb'), encoding='latin1')
 
-    # Anatomy 101: https://en.wikipedia.org/wiki/Metacarpophalangeal_joint
-    # MCP = your kunckle
-    # PIP = the kunckle after your knuckle
-    # DIP = just before the fingernail.
+      self.V = manoRight['v_template']          # Vertices of template model (V), Shape=(778, 3), type=uint32
+      self.F = manoRight['f']                   # Faces of the model (F), Shape=(1538, 3), type=uint32
+      self.K = manoRight['kintree_table'][0]   # Kinematic tree defining the parent joint (K), Shape=(16,), type=int64
+      self.S = manoRight['shapedirs']           # Shape blend shapes that are learned (S), Shape=(778, 3, 10), type=float64
+      self.P = manoRight['posedirs']            # Pose blend shapes that are learned (P), Shape=(778, 3, 135), type=float64
+      self.J = manoRight['J_regressor']         # Joint regressor that are learned (J), Shape=(16,778), type=float64
+      self.W = manoRight['weights']             # Weights that are learned (W), Shape=(778,16), type=float64
+      self.C = manoRight['hands_components']    # Components of hand PCA (C), Shape=(45, 45), type=float64
+      self.M = manoRight['hands_mean']          # Mean hand PCA pose (M), Shape=(45,), type=float64
 
-    '''
-    NOTE(Noah): So here's what's going on here. MANO defines a standard convention for the order of the joints,
-    Then the authors of MobileHand decided to switch things up.
-    '''
-    # [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15] joint order (total 16 joints including wrist)
-    # MANO joint convention:
-    # Note: The order of ring and little finger is swapped!
-    # T   I  M  R   L (Thumb, Index, Middle, Ring, Little)
-    # 16 17 18  19 20 (Fingetip newly added)
-    # 15  3  6  12  9 (DIP)
-    # 14  2  5  11  8 (PIP)
-    # 13  1  4  10  7 (MCP)
-    #       0 (Wrist)
+      # Convert loaded params to Numpy
+      self.V = np.array(self.V, dtype=np.float32)
+      self.F = np.array(self.F, dtype=np.int32) # Need to convert from uint32 to int32 to allow interation in v0 = vertices[:, self.F[:,0],:] # [bs, 1538, 3]    
+      self.S = np.array(self.S, dtype=np.float32)
+      self.P = np.array(self.P, dtype=np.float32)
+      self.J = np.array(self.J.todense(), dtype=np.float32) # Need to convert sparse to dense matrix
+      self.W = np.array(self.W, dtype=np.float32)
+      self.C = np.array(self.C, dtype=np.float32) # Use PCA
+      self.M = np.array(self.M, dtype=np.float32)
+      self.K[0] = -1 # Convert undefined parent of root joint to -1
+      
+      # Reshape the pose blend shapes from (778, 3, 135) -> (778*3, 135) -> (135, 778*3)
+      num_pose_basis = self.P.shape[-1]
+      P = np.reshape(self.P, [-1, num_pose_basis]).T
 
-    '''NEW ORDER, and supposedly the standard convention?'''
-    # Rearrange MANO joint convention to standard convention:
-    #  0:Wrist,
-    #  1:TMCP,  2:TPIP,  3:TDIP,  4:TTIP (Thumb)
-    #  5:IMCP,  6:IPIP,  7:IDIP,  8:ITIP (Index)
-    #  9:MMCP, 10:MPIP, 11:MDIP, 12:MTIP (Middle)
-    # 13:RMCP, 14:RPIP, 15:RDIP, 16:RTIP (Ring)
-    # 17:LMCP, 18:LPIP, 19:LDIP, 20:LTIP (Little)
-    self.remap_joint = [ 0,          # Wrist
-                        13,14,15,16, # Thumb
-                          1, 2, 3,17, # Index
-                          4, 5, 6,18, # Middle
-                        10,11,12,19, # Ring
-                          7, 8, 9,20] # Little
-    
+      # Anatomy 101: https://en.wikipedia.org/wiki/Metacarpophalangeal_joint
+      # MCP = your kunckle
+      # PIP = the kunckle after your knuckle
+      # DIP = just before the fingernail.
 
-    # we note that zmat exists to map some smaller pose space to the 
-    # space of 45 params that MANO expects.
-    self.Z = self.generate_Zmat(self.S, self.V, self.J)
+      '''
+      NOTE(Noah): So here's what's going on here. MANO defines a standard convention for the order of the joints,
+      Then the authors of MobileHand decided to switch things up.
+      '''
+      # [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15] joint order (total 16 joints including wrist)
+      # MANO joint convention:
+      # Note: The order of ring and little finger is swapped!
+      # T   I  M  R   L (Thumb, Index, Middle, Ring, Little)
+      # 16 17 18  19 20 (Fingetip newly added)
+      # 15  3  6  12  9 (DIP)
+      # 14  2  5  11  8 (PIP)
+      # 13  1  4  10  7 (MCP)
+      #       0 (Wrist)
 
-    #########################
-    ### Define pose limit ###
-    #########################
-    # Values adapted from
-    # HOnnotate: A method for 3D Annotation of Hand and Object Poses Supplementary Material
-    # https://www.tugraz.at/fileadmin/user_upload/Institute/ICG/Documents/team_lepetit/images/hampali/supplementary.pdf
-    self.plim = np.array([
-        # Index
-        [[ 0.00, 0.45], [-0.15, 0.20], [0.10, 1.80]], # MCP
-        [[-0.30, 0.20], [ 0.00, 0.00], [0.00, 0.20]], # PIP
-        [[ 0.00, 0.00], [ 0.00, 0.00], [0.00, 1.25]], # DIP
-        # Middle
-        [[ 0.00, 0.00], [-0.15, 0.15], [0.10, 2.00]], # MCP
-        [[-0.50,-0.20], [ 0.00, 0.00], [0.00, 2.00]], # PIP
-        [[ 0.00, 0.00], [ 0.00, 0.00], [0.00, 1.25]], # DIP
-        # Little
-        [[-1.50,-0.20], [-0.15, 0.60], [-0.10, 1.60]], # MCP
-        [[ 0.00, 0.00], [-0.50, 0.60], [ 0.00, 2.00]], # PIP
-        [[ 0.00, 0.00], [ 0.00, 0.00], [ 0.00, 1.25]], # DIP
-        # Ring
-        [[-0.50,-0.40], [-0.25, 0.10], [0.10, 1.80]], # MCP
-        [[-0.40,-0.20], [ 0.00, 0.00], [0.00, 2.00]], # PIP
-        [[ 0.00, 0.00], [ 0.00, 0.00], [0.00, 1.25]], # DIP
-        # Thumb
-        [[ 0.00, 2.00], [-0.83, 0.66], [ 0.00, 0.50]], # MCP
-        [[-0.15,-1.60], [ 0.00, 0.00], [ 0.00, 0.50]], # PIP
-        [[ 0.00, 0.00], [-0.50, 0.00], [-1.57, 1.08]]])# DIP
+      '''NEW ORDER, and supposedly the standard convention?'''
+      # Rearrange MANO joint convention to standard convention:
+      #  0:Wrist,
+      #  1:TMCP,  2:TPIP,  3:TDIP,  4:TTIP (Thumb)
+      #  5:IMCP,  6:IPIP,  7:IDIP,  8:ITIP (Index)
+      #  9:MMCP, 10:MPIP, 11:MDIP, 12:MTIP (Middle)
+      # 13:RMCP, 14:RPIP, 15:RDIP, 16:RTIP (Ring)
+      # 17:LMCP, 18:LPIP, 19:LDIP, 20:LTIP (Little)
+      self.remap_joint = [ 0,          # Wrist
+                          13,14,15,16, # Thumb
+                            1, 2, 3,17, # Index
+                            4, 5, 6,18, # Middle
+                          10,11,12,19, # Ring
+                            7, 8, 9,20] # Little
+      
 
-    ################################
-    ### Define joint angle limit ###
-    ################################
-    self.alim = np.array([
-        # MCP a/a, MCP f/e, PIP f/e, DIP f/e
-        [-20,30], [-10,90], [-1,90], [-1,90], # Index [min,max]
-        [-20,10], [-10,90], [-1,90], [-1,90], # Middle
-        [-20,10], [-10,90], [-1,90], [-1,90], # Ring
-        [-30,20], [-10,90], [-1,90], [-1,90], # Little
-        # x-axis, y-axis  , z-axis
-        # [-45,140], [-45,45], [-45,45], # Thumb TM
-        [-45,45], [-45,45], [-45,45], # Thumb TM
-        [-45, 45], [-45,45], [-45,45], # Thumb MCP
-        [-1 , 90]])                    # Thumb IP flex/ext
-    # Convert degrees to radians
-    self.alim = np.radians(self.alim)
+      # we note that zmat exists to map some smaller pose space to the 
+      # space of 45 params that MANO expects.
+      self.Z = self.generate_Zmat(self.S, self.V, self.J)
 
-    # self.ReLU = 
+      #########################
+      ### Define pose limit ###
+      #########################
+      # Values adapted from
+      # HOnnotate: A method for 3D Annotation of Hand and Object Poses Supplementary Material
+      # https://www.tugraz.at/fileadmin/user_upload/Institute/ICG/Documents/team_lepetit/images/hampali/supplementary.pdf
+      self.plim = np.array([
+          # Index
+          [[ 0.00, 0.45], [-0.15, 0.20], [0.10, 1.80]], # MCP
+          [[-0.30, 0.20], [ 0.00, 0.00], [0.00, 0.20]], # PIP
+          [[ 0.00, 0.00], [ 0.00, 0.00], [0.00, 1.25]], # DIP
+          # Middle
+          [[ 0.00, 0.00], [-0.15, 0.15], [0.10, 2.00]], # MCP
+          [[-0.50,-0.20], [ 0.00, 0.00], [0.00, 2.00]], # PIP
+          [[ 0.00, 0.00], [ 0.00, 0.00], [0.00, 1.25]], # DIP
+          # Little
+          [[-1.50,-0.20], [-0.15, 0.60], [-0.10, 1.60]], # MCP
+          [[ 0.00, 0.00], [-0.50, 0.60], [ 0.00, 2.00]], # PIP
+          [[ 0.00, 0.00], [ 0.00, 0.00], [ 0.00, 1.25]], # DIP
+          # Ring
+          [[-0.50,-0.40], [-0.25, 0.10], [0.10, 1.80]], # MCP
+          [[-0.40,-0.20], [ 0.00, 0.00], [0.00, 2.00]], # PIP
+          [[ 0.00, 0.00], [ 0.00, 0.00], [0.00, 1.25]], # DIP
+          # Thumb
+          [[ 0.00, 2.00], [-0.83, 0.66], [ 0.00, 0.50]], # MCP
+          [[-0.15,-1.60], [ 0.00, 0.00], [ 0.00, 0.50]], # PIP
+          [[ 0.00, 0.00], [-0.50, 0.00], [-1.57, 1.08]]])# DIP
 
-    print('MANO Differentiable Layer Loaded')
-  
-  
+      ################################
+      ### Define joint angle limit ###
+      ################################
+      self.alim = np.array([
+          # MCP a/a, MCP f/e, PIP f/e, DIP f/e
+          [-20,30], [-10,90], [-1,90], [-1,90], # Index [min,max]
+          [-20,10], [-10,90], [-1,90], [-1,90], # Middle
+          [-20,10], [-10,90], [-1,90], [-1,90], # Ring
+          [-30,20], [-10,90], [-1,90], [-1,90], # Little
+          # x-axis, y-axis  , z-axis
+          # [-45,140], [-45,45], [-45,45], # Thumb TM
+          [-45,45], [-45,45], [-45,45], # Thumb TM
+          [-45, 45], [-45,45], [-45,45], # Thumb MCP
+          [-1 , 90]])                    # Thumb IP flex/ext
+      # Convert degrees to radians
+      self.alim = np.radians(self.alim)
+
+      # self.ReLU = 
+
+      print('MANO Differentiable Layer Loaded')
+
+    except:
+      print("Unable to find MANO_RIGHT.pkl")
+
   # NOTE(Noah): I have absolutely no idea how this works. But, I am just going to take it as
   # it is, because this is just pure numpy, Python, and opencv code. No PyTorch in sight here.
   def generate_Zmat(self, S, V, J):
