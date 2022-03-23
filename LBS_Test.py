a55a3bdff67602f52b2030cece461ec5ad707d4c
Lucas Coster <lucascoster90@gmail.com>
2022-03-23 17:58:39 -0400

    Vertices and Blend shape

--- LBS_Test.py
+++ LBS_Test.py
@@ -1,13 +1,23 @@
 # Authored by Lucas Coster
 # Test for the MANO Linear Blend Skinning functions
 
-import tensorFlow as tf
+import tensorflow as tf
 import numpy as np
-import cv2
 from tensorflow.keras import Model
 import os
 import pickle
-import open3d as o3d
 import time
 
-#change 
+def lbs(beta, pose, J_, P_, K_, W_, S_, V_):
+    
+    v_shaped =V_ + blend_shape(beta, S_)
+
+    j_rest = vertices2joints(v_shaped, J_)
+
+def vertices2joints(vert, J_):
+
+    return tf.einsum ('bvt,jv->bjt', vert, J_)
+
+def blend_shape(beta, S_):
+
+    return tf.einsum ('bvt,jv->bjt', beta, S_)
