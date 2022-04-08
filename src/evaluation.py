e2ab4d8200a36a09969bd346e8b1e82e4de620c5
BluBloos <cnoah1705@gmail.com>
2022-04-08 10:04:15 -0400

    Further prepared subroutine headers in evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -17,3 +17,16 @@ def time_model(model, rhd_eval_dir, download_image):
 def evaluate_model(model, rhd_eval_dir, download_image, y_test, y_is_rh):
     pass
 
+# checkpoint_dir is a directory that contains the model checkpoints to iterate over.
+# model is a param for the callable tensorflow model (with loaded weights).
+# rhd_eval_dir is a directory that contains every single evaluation image (of just single hands).
+# download_image is a function that we can call (we pass it one param: fileName), and 
+#   it will return to us a numpy array for the image.
+# y_test is a numpy array with the 3D keypoints for every single image in the RHD evaluation set.
+#   the indices into this array are the names of the image files.
+# y_is_rh is a numpy array that contains either True or False. y_is_rh[i] is True if the annotation at the ith
+#   index in y_test is for a right hand.
+def generate_loss_graph(checkpoint_dir, model, rhd_eval_dir, download_image, y_test, y_is_rh):
+    pass
+
+
