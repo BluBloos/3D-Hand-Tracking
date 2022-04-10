f356d7a6bbd0d9ff0b6b96568383559554d6d740
Max Vincent <maxime@cogeco.ca>
2022-04-10 13:37:03 -0400

    speed

--- src/rodrigues.py
+++ src/rodrigues.py
@@ -4,27 +4,15 @@ import numpy as np
 import time
 
 def rodrigues(rvector):
-    start = time.time()
     batch_size = rvector.shape[0]
     angle = tf.norm(rvector, axis = 1, keepdims= True)
-    print(cstr("rodrigues_angle"), angle)
+    value = tf.fill((batch_size,1), 1e-8)
+    angle = tf.math.add(angle, value)
+
 
     sin = tf.expand_dims(tf.math.sin(angle), axis = 2)
     cos = tf.expand_dims(tf.math.cos(angle), axis = 2)
-    angles = tf.unstack(angle, axis = 0)
-    where = tf.transpose(tf.where(angles == tf.constant([0])))[0]
-    print('\n',where)
-    print('\n',where.shape)
-    
-    for i in where:
-        angles[i] = tf.constant([1.0])
-    
-    # print('\n', angles)
-    
-    # for i in range(batch_size):
-    #     angles[i] = tf.cond(angles[i] == 0, true_fn=lambda:tf.constant([1.0]), false_fn=lambda:angles[i])
-    angle = tf.stack(angles) 
-    print('\n', angle)
+   
     runit = rvector/angle
     
     
@@ -33,8 +21,6 @@ def rodrigues(rvector):
     K = tf.reshape(tf.concat([zero, -kz, ky, kz, zero, -kx, -ky, kx, zero], axis = 1),[batch_size, 3, 3])
     I = tf.eye(3, batch_shape = [batch_size])
     rotationMatrix = I + sin * K + (1 - cos) * tf.linalg.matmul(K, K) 
-    end = time.time()
-    print(end-start)
     return rotationMatrix
 
     
