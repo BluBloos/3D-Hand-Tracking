548d0738a4482c4c31368c535fde9becad60b08a
BluBloos <cnoah1705@gmail.com>
2022-04-24 09:19:13 -0400

    Improved function header for evaluation_model

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,8 +7,9 @@ def time_model(model, rhd_eval_dir, download_image):
 
 
 # model is a param for the callable tensorflow model (with loaded weights).
-# rhd_eval_dir is a directory that contains every single evaluation image (of just single hands).
-# download_image is a function that we can call (we pass it one param: fileName), and 
+# rhd_eval_dir is a directory name for a directory 
+#   that contains every single evaluation image (of just single hands).
+# download_image is a function that we can call (we pass it one param, fileName), and 
 #   it will return to us a numpy array for the image.
 # y_test is a numpy array with the 3D keypoints for every single image in the RHD evaluation set.
 #   the indices into this array are the names of the image files.
