281776f40a795f87a1f1c816a4ee4358dbaaebd3
BluBloos <cnoah1705@gmail.com>
2022-05-06 07:39:24 -0400

    added DROP_PROB

--- src/ireg_layer.py
+++ src/ireg_layer.py
@@ -6,15 +6,16 @@ import tensorflow as tf
 def MAKE_REGRESSION_MODULE(batch_size):
   
   MODEL_OUPUTS = 55 + 3 # +3 for the camera rot matrix param (3 = angle axis vector). 
+  DROP_PROB = 0.4
   
   inputs = tf.keras.layers.Input(shape=[576,])
   
   model = tf.keras.models.Sequential()
   model.add(tf.keras.Input(shape = (576 + MODEL_OUPUTS,)))
   model.add(tf.keras.layers.Dense(288, activation = 'relu'))
-  model.add(tf.keras.layers.Dropout(0.5))
+  model.add(tf.keras.layers.Dropout(DROP_PROB))
   model.add(tf.keras.layers.Dense(288, activation = 'relu'))
-  model.add(tf.keras.layers.Dropout(0.5))
+  model.add(tf.keras.layers.Dropout(DROP_PROB))
   model.add(tf.keras.layers.Dense(MODEL_OUPUTS))
   
   params = tf.zeros((batch_size, MODEL_OUPUTS))
