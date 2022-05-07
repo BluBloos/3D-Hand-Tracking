a123733564aa0c83061b84122056831c66d59f86
BluBloos <cnoah1705@gmail.com>
2022-05-07 00:49:56 -0400

    Added improved 3D_PCK curve + AUC calculation

--- src/evaluation.py
+++ src/evaluation.py
@@ -14,6 +14,15 @@ from mobilehand_lfuncs import distance
 def time_model(model, rhd_eval_dir, download_image):
     pass
 
+# x and y should be standard lists!
+def get_auc(x, y):
+    result = 0
+    for i in range(len(x)-1):
+        delta_x = 1 / len(x)
+        delta_y = y[i+1] - y[i]
+        result += 0.5 * delta_x * delta_y # add triangle area.
+        result += delta_x * y[i] # rectangle @ bottom.
+    return round(result, 3)    
 
 # model is a param for the callable tensorflow model (with loaded weights).
 # rhd_eval_dir is a directory name for a directory 
@@ -55,12 +64,35 @@ def evaluate_model(model, rhd_eval_dir, set, download_image, y_test, gcs_path):
     thresholds = tf.stack(thresholds)
     thresholds = thresholds[:, 0] * 1000
     print(thresholds)
-    pck_graph = plt.axes()
-    pck_graph.grid()
-    pck_graph.plot(thresholds, percentage, label = 'Our model')
+    
+    plt.figure(figsize=(10.0, 8.0))
+    plt.grid()
+
+    _thresholds = [20, 25, 30, 35, 40, 45, 50]
+    plt.plot(_thresholds, percentage, marker='x', label='Ours (AUC={})'.format(
+        get_auc(_thresholds, percentage)
+    ))
+    
+    __thresholds = [20, 23.33, 26.66, 30, 33.33, 36.66, 40, 43.33, 46.66, 50]
+    vals = [0.75, 0.8, 0.84, 0.88, 0.9, 0.92, 0.94, 0.95, 0.96, 0.97]
+    plt.plot(__thresholds, vals, linestyle=':', label = 'Zhang et al. (AUC={})'.format(
+        get_auc(__thresholds, vals)
+    ))
+    vals =  [0.79, 0.87, 0.92, 0.95, 0.97, 0.98, 0.99]
+    plt.plot(_thresholds, vals, marker='d', label = 'Baek et al. (AUC={})'.format(
+        get_auc(_thresholds, vals)
+    ))
+    vals = [0.805, 0.87, 0.91, 0.93, 0.95, 0.955, 0.97] 
+    plt.plot(_thresholds, vals, marker='s', label = 'Ge et al. (AUC={})'.format(
+        get_auc(_thresholds, vals)
+    ))
+
     plt.title('RHD Dataset 3D PCK Curve')
     plt.xlabel('Error Thresholds (mm)')
     plt.ylabel('Percentage of Correct Keypoints')
+
+    plt.legend(loc="lower right")
+
     plt.show()
     
 
